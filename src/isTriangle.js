7df6c297acbb1a6e5fff36d80267e86ed4cb68e3
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-11-01 00:55:52 +0530

    is triangle done

---
+++ src/isTriangle.js
@@ -0,0 +1,12 @@
+const input = document.querySelectorAll(".angle");
+const checkBtn = document.querySelector("#check-btn");
+const outputText = document.querySelector("#output");
+
+function clickHandler() {
+    sum=Number(input[0].value)+Number(input[1].value)+Number(input[2].value);
+    if (sum === 180)
+        outputText.innerHTML="Yes, the angles form a triangle.";
+    else outputText.innerHTML="These angles do not form a triangle.";
+}
+
+checkBtn.addEventListener("click", clickHandler)
