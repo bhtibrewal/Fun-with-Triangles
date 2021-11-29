f5b96d2457803e3ce62dbab9b8861523b6c3471b
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-11-29 13:21:29 +0530

    ADD: ques to quiz

--- src/quiz.js
+++ src/quiz.js
@@ -2,7 +2,7 @@ const submit = document.querySelector("#btn-primary");
 const quizForm = document.querySelector(".quiz-form");
 const output = document.querySelector(".output");
 
-const correctAns = ["90", "right angled"];
+const correctAns = ["90", "right angled", "60"];
 
 function calculateScore() {
 
@@ -14,8 +14,8 @@ function calculateScore() {
         if (value === correctAns[index])
             score++;
         index++;
-        
+
     }
-    output.innerHTML="Your score is : "+ score + " Correct Answers Question 1 = 90°, Question 2 = right angled";
+    output.innerHTML = "Your score is : " + score + " Correct Answers Question 1 = 90°, Question 2 = right angled, Question 3 = 60°";
 }
 submit.addEventListener("click", calculateScore);
