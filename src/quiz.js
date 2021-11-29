df1357c6b5b0d1e038addb9e279db839bc779f2e
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-11-29 15:34:01 +0530

    fix

--- src/quiz.js
+++ src/quiz.js
@@ -1,26 +1,34 @@
 const submit = document.querySelector("#btn-primary");
 const quizForm = document.querySelector(".quiz-form");
 const output = document.querySelector(".output");
+const radios = document.querySelectorAll("input[type=radio]:checked");
 
-const correctAns = ["90", "right angled", "60"];
+
+const correctAns = ["90", "right angled", "60", "a+b+c", "40"];
 
 function calculateScore() {
-
+    const radios = document.querySelectorAll("input[type=radio]:checked");  
+    
     var score = 0;
     var index = 0;
     const formResults = new FormData(quizForm);
+    if (radios.length === correctAns.length) {
+        for (let value of formResults.values()) {
 
-    for (let value of formResults.values()) {
-        if (value === "") alert('fill the form');
+            if (value === correctAns[index])
+                score++;
+            index++;
 
-        if (value === correctAns[index])
-            score++;
-        index++;
-
-    }
-    output.innerHTML = `Your score is : ${score} 
+        }
+        output.innerHTML = `Your score is : ${score} 
     Correct Answers: Question 1 = 90°,
     Question 2 = right angled,
-    Question 3 = 60°`;
+    Question 3 = 60°
+    Question 4 = a+b+c
+    Question5 = 40
+    `;
+    }
+    else alert("Its Compulsory to attempt all the ques..");
 }
+console.log(radios);
 submit.addEventListener("click", calculateScore);
