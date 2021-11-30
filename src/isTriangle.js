59b23f869a60f22dc58daba85d3179083a766812
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-11-30 14:51:56 +0530

    isTriangle -ve value fix

--- src/isTriangle.js
+++ src/isTriangle.js
@@ -3,10 +3,14 @@ const checkBtn = document.querySelector("#check-btn");
 const outputText = document.querySelector("#output");
 
 function clickHandler() {
-    sum=Number(input[0].value)+Number(input[1].value)+Number(input[2].value);
-    if (sum === 180)
-        outputText.innerHTML="Yes, the angles form a triangle.";
-    else outputText.innerHTML="These angles do not form a triangle.";
+    if (Number(input[0].value <= 0) || Number(input[1].value <= 0) || Number(input[2].value <= 0))
+        alert("Angles should be greater 0");
+    else {
+        sum = Number(input[0].value) + Number(input[1].value) + Number(input[2].value);
+        if (sum === 180)
+            outputText.innerHTML = "Yes, the angles form a triangle.";
+        else outputText.innerHTML = "These angles do not form a triangle.";
+    }
 }
 
 checkBtn.addEventListener("click", clickHandler)
